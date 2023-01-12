cd5cd0cb58acdef63e549cf8e00fc6fde45cd0b5
Carles Rafols <41967813+karb94@users.noreply.github.com>
2023-01-12 13:37:23 +0000

    Fast push

---
+++ vm-hardware-configuration.nix
@@ -0,0 +1,9 @@
+{ modulesPath, ... }: {
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+
+  boot.initrd.availableKernelModules =
+    [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+}
