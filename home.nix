1fab95416a91ee2253562b0900a9208283275654
Carles Rafols <41967813+karb94@users.noreply.github.com>
2023-01-13 18:56:02 +0000

    Fast push

--- home.nix
+++ home.nix
@@ -4,6 +4,17 @@ let
   ln = lib.file.mkOutOfStoreSymlink;
   dotfiles="${xdg.configHome}/dotfiles";
   dotconfig = "${dotfiles}/.config";
+  # link = parentFolder: path: 
+  p = [
+    "alacritty/alacritty.yml"
+    "bspwm/bspwmrc"
+  ]
+  # f = commonDir: path: {name="${path}"; value={source=("${commonDir}/${path}");};}
+  mkSymlink = commonDir: 
+  let
+    f = path: {name="${path}"; value={source=("${commonDir}/${path}");};}
+  in
+    paths: builtins.listToAttrs (map f paths)
 in
   {
     # Home Manager needs a bit of information about you and the
@@ -13,8 +24,11 @@ in
 
     home.stateVersion = "22.11";
 
-    home.file."hm".source = ln ./test_dir/test;
-    xdg.configFile = {
-      "alacritty".source = ln "${dotconfig}/alacritty";
-    };
+    
+    # home.file."hm".source = ln ./test_dir/test;
+    xdg.configFile = mkSymlink "${dotconfig}" p;
+    # xdg.configFile = {
+    #   "alacritty/alacritty.yml".source = ln "${dotconfig}/alacritty/alacritty.yml";
+    #   "bspwm/bspwmrc".source = ln "${dotconfig}/bspwm/bspwmrc";
+    # };
   }
