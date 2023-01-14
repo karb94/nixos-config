29ef6f096d74a29328131b2bb822cccb24edadf8
Carles Rafols <41967813+karb94@users.noreply.github.com>
2023-01-14 09:27:28 +0000

    Fast push

--- home.nix
+++ home.nix
@@ -48,4 +48,13 @@ in
     home.stateVersion = "22.11";
 
     xdg.configFile = mkSymlinks "${dotConfigDir}" dotConfigFilesToLink;
+
+    programs.bash.bashrcExtra = ''
+    # If not running interactively, don't do anything
+    [[ $- != *i* ]] && return
+
+    # Source extra configuration
+    test -f $HOME/.config/bash/config.bash && source $_
+    ''
+
   }
