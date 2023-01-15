7afaadaf0fefb8c5164ef9d3274ae5350febd8e8
Carles Rafols <41967813+karb94@users.noreply.github.com>
2023-01-15 21:05:24 +0000

    Fast push

--- home.nix
+++ home.nix
@@ -12,8 +12,6 @@ let
     in
       paths: builtins.listToAttrs (map f paths);
 
-  userName = "carles";
-  homeDir = "/home/${userName}";
   HomeFilesToLink = [
     ".bashrc"
     ".inputrc"
@@ -45,16 +43,8 @@ in
     home.stateVersion = "22.11";
     home.username = "carles";
     home.homeDirectory = "/home/carles";
-    home.sessionVariables = {
-      EDITOR = "nvim";
-    };
-    home.sessionsPath = [
-      "$HOME/.local/bin"
-      "$HOME/.local/scripts"
-    ]
 
     home.file = mkSymlinks "" HomeFilesToLink;
-    # home.file.".xinitrc".source = lib.file.mkOutOfStoreSymlink "${homeDir}/.xinitrc";
 
     xdg.configFile = mkSymlinks ".config/" dotConfigFilesToLink;
 
