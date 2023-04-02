1d9e0ca78464f40d8a9943a71e96ec21ebd3fbd8
Carles Rafols <41967813+karb94@users.noreply.github.com>
2023-04-02 17:51:46 +0100

    Fast push

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager {
-            extraSpecialArgs = { inherit inputs; };
+            home_manager.extraSpecialArgs = { inherit inputs; };
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.carles = import ./home.nix;
