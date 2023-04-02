0359f2b782c21a4f6adc0df69d5b2cd25e10bca0
Carles Rafols <41967813+karb94@users.noreply.github.com>
2023-04-02 16:59:13 +0100

    Fast push

--- configuration.nix
+++ configuration.nix
@@ -63,9 +63,9 @@
 
   # Locale
   i18n.supportedLocales = [
-    "en_GB.UTF-8/UTF-8"
-    "es_ES.UTF-8/UTF-8"
-    "ca_ES.UTF-8/UTF-8"
+    "en_GB.UTF-8"
+    "es_ES.UTF-8"
+    "ca_ES.UTF-8"
   ];
   i18n.defaultLocale = "en_GB.UTF-8 UTF-8";
 
