5ac133ceda296c7258d30a1167554bf0b69ba93a
Carles Rafols <41967813+karb94@users.noreply.github.com>
2023-04-02 17:01:49 +0100

    Fast push

--- configuration.nix
+++ configuration.nix
@@ -63,11 +63,11 @@
 
   # Locale
   i18n.supportedLocales = [
-    "en_GB.UTF-8"
-    "es_ES.UTF-8"
-    "ca_ES.UTF-8"
+    "en_GB.UTF-8/UTF-8"
+    "es_ES.UTF-8/UTF-8"
+    "ca_ES.UTF-8/UTF-8"
   ];
-  i18n.defaultLocale = "en_GB.UTF-8 UTF-8";
+  i18n.defaultLocale = "en_GB.UTF-8";
 
   fonts.fonts = with pkgs; [
     (nerdfonts.override { fonts = [ "FiraCode" ]; })
