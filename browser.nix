ba568258fc8df5986d2c0fc359ec4de9c3ad3408
Carles Rafols <41967813+karb94@users.noreply.github.com>
2023-04-06 15:04:35 +0100

    Fast push

--- browser.nix
+++ browser.nix
@@ -2,7 +2,9 @@
   config.programs.chromium = {
     enable = true;
     extensions = [
-      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # Bitwarden
+      "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
+      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
+      "dbepggeogbaibhgnhhndojpepiihcmeb" # Vimium
       "dcpihecpambacapedldabdbpakmachpb;https://raw.githubusercontent.com/iamadamdev/bypass-paywalls-chrome/master/src/updates/updates.xml"   # Bypass paywalls
     ];
     extraOpts = {
