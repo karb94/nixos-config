4e857ae6a3ea61e7f2e765152e0f20217d8051e7
Carles Rafols <41967813+karb94@users.noreply.github.com>
2023-04-16 16:52:58 +0000

    Fast push

--- home.nix
+++ home.nix
@@ -40,16 +40,6 @@ let
     "zathura"
   ];
   configSources = mkSymlinks ".config/" configFilesToSymlink;
-
-  mkAppSymlinks = commonDir: 
-  let
-  ln = lib.file.mkOutOfStoreSymlink;
-  f = path: {
-    name = "${path}";
-    value = {source = ln "${sourcesDir}/${commonDir}${path}";};
-  };
-in
-paths: builtins.listToAttrs (map f paths);
 in
   {
     home.stateVersion = "22.11";
@@ -61,4 +51,5 @@ in
     xdg.configFile = configSources;
     xdg.dataFile = {
       "applications/brave-browser.desktop".source = lib.file.mkOutOfStoreSymlink "${pkgs.brave}/share/applications/brave-browser.desktop";
-  }
+    };
+}
