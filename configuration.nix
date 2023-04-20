f4573008bb6996e71eac6ad3797c268c4d0c3689
Carles Rafols <41967813+karb94@users.noreply.github.com>
2023-04-20 21:46:38 +0000

    Fast push

--- configuration.nix
+++ configuration.nix
@@ -86,7 +86,7 @@
 
   # Enable doas instead of sudo
   security.doas.enable = true;
-  # security.sudo.enable = false;
+  security.sudo.enable = false;
   # Configure doas
   security.doas.extraRules = [
     {
