dfd483c82ebb8f70a563e02ded849a5b4ae9b386
Carles Rafols <41967813+karb94@users.noreply.github.com>
2023-04-23 12:32:53 +0000

    Fast push

--- home.nix
+++ home.nix
@@ -1,17 +1,19 @@
-{ config, pkgs, ... }:
-with config;
-let
+{
+  config,
+  pkgs,
+  ...
+}:
+with config; let
   repoName = "dotfiles";
   sourcesDir = "${xdg.configHome}/${repoName}";
-  mkSymlinks = commonDir: 
-    let
-      ln = lib.file.mkOutOfStoreSymlink;
-      f = path: {
-        name = "${path}";
-        value = {source = ln "${sourcesDir}/${commonDir}${path}";};
-      };
-    in
-      paths: builtins.listToAttrs (map f paths);
+  mkSymlinks = commonDir: let
+    ln = lib.file.mkOutOfStoreSymlink;
+    f = path: {
+      name = "${path}";
+      value = {source = ln "${sourcesDir}/${commonDir}${path}";};
+    };
+  in
+    paths: builtins.listToAttrs (map f paths);
   homeFilesToSymlink = [
     ".bashrc"
     ".inputrc"
@@ -29,29 +31,29 @@ let
     "git"
     "lf"
     "mpv"
-    "newsboat/urls"
-    "newsboat/config"
+    "newsboat"
+    "newsboat"
     "nvim/after"
     "nvim/init.vim"
     "nvim/lua"
     "picom"
     "rofi"
     "sxhkd"
+    "xob"
     "youtube-dl"
     "zathura"
   ];
   configSources = mkSymlinks ".config/" configFilesToSymlink;
-in
-  {
-    home.stateVersion = "22.11";
-    home.username = "carles";
-    home.homeDirectory = "/home/carles";
+in {
+  home.stateVersion = "22.11";
+  home.username = "carles";
+  home.homeDirectory = "/home/carles";
 
-    home.file = homeSources;
-    xdg.enable = true; # track XDG files and directories
-    xdg.configFile = configSources;
-    xdg.dataFile = {
-      "applications/brave-browser.desktop".source = lib.file.mkOutOfStoreSymlink "${pkgs.brave}/share/applications/brave-browser.desktop";
-      "applications/spotify.desktop".source = lib.file.mkOutOfStoreSymlink "${pkgs.spotify}/share/applications/spotify.desktop";
-    };
+  home.file = homeSources;
+  xdg.enable = true; # track XDG files and directories
+  xdg.configFile = configSources;
+  xdg.dataFile = {
+    "applications/brave-browser.desktop".source = lib.file.mkOutOfStoreSymlink "${pkgs.brave}/share/applications/brave-browser.desktop";
+    "applications/spotify.desktop".source = lib.file.mkOutOfStoreSymlink "${pkgs.spotify}/share/applications/spotify.desktop";
+  };
 }
