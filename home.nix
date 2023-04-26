7d49b8684039220a51cd85eaf20e5ae8570f639a
Carles Rafols <41967813+karb94@users.noreply.github.com>
2023-04-26 22:39:27 +0000

    Fast push

--- home.nix
+++ home.nix
@@ -12,8 +12,8 @@ with config; let
       name = "${path}";
       value = {source = ln "${sourcesDir}/${commonDir}${path}";};
     };
-  in
-    paths: builtins.listToAttrs (map f paths);
+  in paths: builtins.listToAttrs (map f paths);
+
   homeFilesToSymlink = [
     ".bashrc"
     ".inputrc"
@@ -44,6 +44,27 @@ with config; let
     "zathura"
   ];
   configSources = mkSymlinks ".config/" configFilesToSymlink;
+
+  linkDesktopApps = let
+    ln = lib.file.mkOutOfStoreSymlink;
+    appStorePath = pkgs."${appName}";
+    f = pkgName: appName: {
+      name = "applications/${appName}.desktop";
+      value = {source = ln "${appStorePath}/share/applications/${appName}.desktop";};
+    };
+    in paths: builtins.listToAttrs (lib.attrsets.mapAttrs' f);
+  desktopApps = {
+    zathura = "org.pwmt.zathura-pdf-mupdf";
+    brave = "brave-browser";
+    spotify = "spotify";
+  };
+  # desktopAppsSources = linkDesktopApps desktopApps;
+  # f = pkgName: appName: {
+  #   name = "applications/${appName}.desktop";
+  #   value = {source = ln "${appStorePath}/share/applications/${appName}.desktop";};
+  # };
+  # desktopApps;
+
 in {
   home.stateVersion = "22.11";
   home.username = "carles";
@@ -52,8 +73,12 @@ in {
   home.file = homeSources;
   xdg.enable = true; # track XDG files and directories
   xdg.configFile = configSources;
+
+  # xdg.dataFile = desktopAppsSources;
+
   xdg.dataFile = {
     "applications/brave-browser.desktop".source = lib.file.mkOutOfStoreSymlink "${pkgs.brave}/share/applications/brave-browser.desktop";
     "applications/spotify.desktop".source = lib.file.mkOutOfStoreSymlink "${pkgs.spotify}/share/applications/spotify.desktop";
+    "applications/org.pwmt.zathura-pdf-mupdf.desktop".source = lib.file.mkOutOfStoreSymlink "${pkgs.zathura}/share/applications/.desktop";
   };
 }
--- packages.nix
+++ packages.nix
@@ -24,6 +24,7 @@ with pkgs; let
     cmake
     coreutils
     fd
+    file
     findutils
     fzf
     git
