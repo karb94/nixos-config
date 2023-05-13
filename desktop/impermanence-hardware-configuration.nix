ca36651e2a6e89c73782f29f61cd333746866797
karb94 <41967813+karb94@users.noreply.github.com>
2023-05-13 18:03:51 +0100

    Update impermanence-hardware-configuration.nix

--- desktop/impermanence-hardware-configuration.nix
+++ desktop/impermanence-hardware-configuration.nix
@@ -41,22 +41,7 @@
       fsType = "vfat";
     };
   };
-  impermanence.nixosModules.impermanence {
-    environment.persistence."/nix/persist/system" = {
-      hideMounts = true;
-      directories = [
-        "/var/log"
-        "/var/lib/bluetooth"
-        "/var/lib/nixos"
-        "/var/lib/systemd/coredump"
-        "/etc/NetworkManager/system-connections"
-      ];
-      files = [
-        "/etc/machine-id"
-        { file = "/etc/nix/id_rsa"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
-      ];
-    };
-  };
+
     # users.talyz = {
     #   directories = [
     #     "Downloads"
