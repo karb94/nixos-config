be38f162af89ceef34d94cad3101bff1dc98bb63
Carles Rafols i Belles <41967813+karb94@users.noreply.github.com>
2023-06-07 08:32:30 +0100

    Fast push

--- common/bootloader.nix
+++ common/bootloader.nix
@@ -5,6 +5,7 @@
 }:
 {
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 20;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 }
