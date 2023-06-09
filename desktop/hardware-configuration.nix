201115b43c581e9226ed8fbeb8db70f3a0604a86
Carles Rafols i Belles <41967813+karb94@users.noreply.github.com>
2023-06-09 23:16:41 +0100

    Fast push

--- desktop/hardware-configuration.nix
+++ desktop/hardware-configuration.nix
@@ -1,8 +1,12 @@
-{ inputs, lib, impermanence, ... }:
-  let
-    luks_name = "luksroot";
-    luks_device = "/dev/mapper/luksroot";
-  in {
+{
+  inputs,
+  lib,
+  impermanence,
+  ...
+}: let
+  luks_name = "luksroot";
+  luks_device = "/dev/mapper/luksroot";
+in {
   # Import generic hardware configuration
   imports = [
     inputs.hardware.nixosModules.common-cpu-amd
@@ -13,89 +17,97 @@
   ];
 
   config = lib.mkMerge [
-  {
-    boot.initrd.availableKernelModules = [
-      "ahci"
+    {
+      boot.initrd.availableKernelModules = [
+        "ahci"
         "xhci_pci"
         "nvme"
         "sd_mod"
         "usbhid"
         "usb_storage"
-    ];
-    boot.kernelModules = [ "kvm-amd" ];
+      ];
+      boot.kernelModules = ["kvm-amd"];
 
-    hardware.enableAllFirmware = lib.mkDefault true;
-
-    swapDevices = [
-    {
-      device = "/.swapfile";
-      size = 20480;  # 20GB
+      hardware.enableAllFirmware = lib.mkDefault true;
     }
-    ];
-  }
-  (
-   lib.mkIf impermanence {
-     boot.initrd.luks.devices."${luks_name}".device = "/dev/disk/by-partlabel/root";
-     fileSystems = {
-    "/" = {
-      device = "none";
-      fsType = "tmpfs";
-      options = [ "size=6G" "mode=755" ];
-      neededForBoot = true;
-    };
-    "/nix" = {
-      device = luks_device;
-      fsType = "btrfs";
-      options = [ "subvol=nix" "compress=zstd" "noatime" ];
-      neededForBoot = true;
-    };
-    "/persist" = {
-      device = luks_device;
-      fsType = "btrfs";
-      options = [ "subvol=persist" "compress=zstd" "noatime" ];
-      neededForBoot = true;
-    };
-    "/swap" = {
-      device = luks_device;
-      fsType = "btrfs";
-      options = [ "subvol=swap" "compress=zstd" "noatime" ];
-      neededForBoot = true;
-    };
-    "/boot" = {
-      device = "/dev/disk/by-partlabel/boot";
-      fsType = "vfat";
-    };
-     };
-     environment.persistence."/persist/system" = {
-       hideMounts = true;
-       directories = [
-         "/var/log"
-           "/var/lib/bluetooth"
-           "/var/lib/nixos"
-           "/var/lib/systemd/coredump"
-           "/etc/NetworkManager/system-connections"
-       ];
-       files = [
-         "/etc/machine-id"
-         { file = "/etc/nix/id_rsa"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
-       ];
-     };
-  }
-  )
+    (
+      lib.mkIf impermanence {
+        boot.initrd.luks.devices."${luks_name}".device = "/dev/disk/by-partlabel/root";
+        fileSystems = {
+          "/" = {
+            device = "none";
+            fsType = "tmpfs";
+            options = ["size=6G" "mode=755"];
+            neededForBoot = true;
+          };
+          "/nix" = {
+            device = luks_device;
+            fsType = "btrfs";
+            options = ["subvol=nix" "compress=zstd" "noatime"];
+            neededForBoot = true;
+          };
+          "/persist" = {
+            device = luks_device;
+            fsType = "btrfs";
+            options = ["subvol=persist" "compress=zstd" "noatime"];
+            neededForBoot = true;
+          };
+          "/swap" = {
+            device = luks_device;
+            fsType = "btrfs";
+            options = ["subvol=swap" "compress=zstd" "noatime"];
+            neededForBoot = true;
+          };
+          "/boot" = {
+            device = "/dev/disk/by-partlabel/boot";
+            fsType = "vfat";
+          };
+        };
+        environment.persistence."/persist/system" = {
+          hideMounts = true;
+          directories = [
+            "/var/log"
+            "/var/lib/bluetooth"
+            "/var/lib/nixos"
+            "/var/lib/systemd/coredump"
+            "/etc/NetworkManager/system-connections"
+          ];
+          files = [
+            "/etc/machine-id"
+            {
+              file = "/etc/nix/id_rsa";
+              parentDirectory = {mode = "u=rwx,g=,o=";};
+            }
+          ];
+        };
+        swapDevices = [
+          {
+            device = "/swap/swapfile";
+            size = 20480; # 20GB
+          }
+        ];
+      }
+    )
     (
       lib.mkIf (! impermanence) {
-      fileSystems = {
-        "/" = {
-          device = "/dev/disk/by-label/root";
-          fsType = "ext4";
+        fileSystems = {
+          "/" = {
+            device = "/dev/disk/by-label/root";
+            fsType = "ext4";
+          };
+          "/boot" = {
+            device = "/dev/disk/by-label/boot";
+            fsType = "vfat";
+          };
         };
-        "/boot" = {
-          device = "/dev/disk/by-label/boot";
-          fsType = "vfat";
-        };
-        };
+
+        swapDevices = [
+          {
+            device = "/.swapfile";
+            size = 20480; # 20GB
+          }
+        ];
       }
     )
   ];
-
 }
