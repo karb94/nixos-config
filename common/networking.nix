842f8a8c4ad75e2d015916b978c044d71ff32ebd
Carles Rafols i Belles <41967813+karb94@users.noreply.github.com>
2023-06-11 19:18:16 +0100

    Fast push

--- common/networking.nix
+++ common/networking.nix
@@ -4,4 +4,5 @@
   networking.networkmanager.enable = true;
   networking.networkmanager.wifi.backend = "iwd";
   networking.hostName = hostname;
+  networking.nameservers = [ "194.242.2.3" "9.9.9.9" ];
 }
