06b86c8f34f5a01c7b62c6516830d7beac4d579c
Carles Rafols <41967813+karb94@users.noreply.github.com>
2023-12-05 22:56:25 +0000

    Fast push

--- common/nix.nix
+++ common/nix.nix
@@ -1,4 +1,5 @@
 # Nix configuration
+{ inputs, ... }:
 {
   nix.settings = {
     # Enable flakes and new 'nix' command
@@ -8,4 +9,6 @@
     # Conform to the XDG Base Directory Specification
     use-xdg-base-directories = true;
   };
+  # Use the system's nixpkgs when using nix commands. E.g., nix run nixpkgs#hello
+  nix.registry.nixpkgs.flake = inputs.nixpkgs;
 }
