17a7c694040fcc3398fc646a64bbb1dfe06b73b2
Carles Rafols <41967813+karb94@users.noreply.github.com>
2024-05-17 00:21:42 +0100

    Fast push

--- desktop/wayland.nix
+++ desktop/wayland.nix
@@ -1,13 +1,12 @@
 {
   inputs,
-  lib,
   pkgs,
   ...
 }: {
 
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    # package = inputs.hyprland.packages.${pkgs.system}.hyprland;
   };
 
   environment.systemPackages = with pkgs; [
