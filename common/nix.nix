7aa14308b517cb423e5d104b6bab014b859ac072
Carles Rafols <41967813+karb94@users.noreply.github.com>
2025-02-19 18:45:11 +0000

    Fast push

--- common/nix.nix
+++ common/nix.nix
@@ -18,8 +18,11 @@
       # Compare to the key published at https://nix-community.org/cache
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
-  
   };
+  nix.extraOptions = ''
+    bash-prompt-prefix = \n[nix develop]
+  '';
+
   # Use the system's nixpkgs when using nix commands. E.g., nix run nixpkgs#hello
   nix.registry.nixpkgs.flake = inputs.nixpkgs;
 }
