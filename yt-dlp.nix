f5d56c433b32bf40cba56280213d444efffc56e9
Carles Rafols <41967813+karb94@users.noreply.github.com>
2025-09-24 07:31:08 +0100

    Fast push

--- yt-dlp.nix
+++ yt-dlp.nix
@@ -3,8 +3,8 @@ final: prev: {
     src = prev.fetchFromGitHub {
       owner = "yt-dlp";
       repo = "yt-dlp";
-      rev = "2025.09.05";
-      hash = "sha256-9y6OUVm6hNTTi5FFmd9DHcmAMrvSmDD+4kDe00aMTDI=";
+      rev = "2025.09.23";
+      hash = "sha256-pqdR1JfiqvBs5vSKF7bBBKqq0DRAi3kXCN1zDvaW3nQ=";
     };
   });
 }
